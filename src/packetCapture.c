79fb514efae0d93cf0ca4782df0576eba2d0814e
TakuKitamura <takukitamura.io@gmail.com>
2021-10-21 09:58:39 +0900

    add timestamp

--- src/packetCapture.c
+++ src/packetCapture.c
@@ -2,16 +2,23 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
-
 #include <net/if.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <errno.h>
+#include <time.h>
 
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
-#include <errno.h>
+struct can_packet
+{
+    canid_t can_id; /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+    __u8 can_dlc;   /* data length code: 0 .. 8 */
+    __u8 data[8] __attribute__((aligned(8)));
+    time_t timestamp;
+};
 
 int getSocket()
 {
@@ -35,13 +42,14 @@ int getSocket()
     return s;
 }
 
-struct can_frame packetCapture(int fd)
+struct can_packet packetCapture(int fd)
 {
-    struct can_frame frame;
+    struct can_packet frame;
     // CANパケットキャプチャ
     while (1)
     {
-        ssize_t n = read(fd, &frame, sizeof(struct can_frame));
+        ssize_t n = read(fd, &frame, sizeof(struct can_packet));
+        time(&frame.timestamp);
         if (n != -1)
         {
             return frame;
@@ -55,7 +63,7 @@ struct can_frame packetCapture(int fd)
     return frame;
 }
 
-void printFrame(struct can_frame frame)
+void printFrame(struct can_packet frame)
 {
     printf("can_id = 0x%x, can_dlc %d, can_data = [", frame.can_id, frame.can_dlc);
     for (int i = 0; i < 8; i++)
@@ -77,7 +85,7 @@ int main(void)
     int fd = getSocket();
     while (1)
     {
-        struct can_frame frame = packetCapture(fd);
+        struct can_packet frame = packetCapture(fd);
         printFrame(frame);
     }
 }
