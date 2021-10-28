e28a2ba676b4e40998611ebb593b7040e41a5084
TakuKitamura <takukitamura.io@gmail.com>
2021-10-28 17:04:42 +0900

    refacta

--- AVATAR_executablecode/rust/src/record_in_edr.rs
+++ AVATAR_executablecode/rust/src/record_in_edr.rs
@@ -15,6 +15,10 @@ pub extern "C" fn record_in_edr(
     indicator: u8,
     door: u8,
 ) -> i32 {
+    let speed_id = 0x1b4;
+    let indicator_id = 0x188;
+    let door_id = 0x19b;
+
     struct EventData {
         timestamp: chrono::NaiveDateTime,
         crashed: bool,
@@ -24,18 +28,24 @@ pub extern "C" fn record_in_edr(
         door: u8,
     }
     static STATIC_EVENT_DATA: Lazy<Mutex<Vec<EventData>>> = Lazy::new(|| Mutex::new(Vec::new()));
-    static STATIC_CRASH_TIMESTAMP: Lazy<Mutex<chrono::NaiveDateTime>> =
-        Lazy::new(|| Mutex::new(NaiveDateTime::from_timestamp(0, 0)));
 
     let mut event_data = STATIC_EVENT_DATA.lock().unwrap();
-    let mut crash_timestamp = STATIC_CRASH_TIMESTAMP.lock().unwrap();
+
     let sec_part: u64 = timestamp / 1000000000; // 整数部
     let nano_part: u64 = timestamp - (sec_part * 1000000000); // 小数部
     let timestamp = NaiveDateTime::from_timestamp(sec_part as i64, nano_part as u32);
 
-    let crashed = *crash_timestamp != NaiveDateTime::from_timestamp(0, 0);
+    let mut crashed_index: i32 = -1;
+    match event_data.iter().position(|x| x.crashed == true) {
+        None => (),
+        Some(i) => {
+            crashed_index = i as i32;
+        }
+    };
 
-    if can_id == 0x1B4 {
+    let crashed = crashed_index != -1;
+
+    if can_id == speed_id {
         // speed
         // 0x10で割る前の値がspeedなので0x10で割る
         let mph_speed: f64 = (speed as f64) / (0x10 as f64);
@@ -49,60 +59,57 @@ pub extern "C" fn record_in_edr(
             door: 0,
         });
 
-        let before_time = event_data[event_data.len() - 1].timestamp;
-
         // EDRへ書き込み
-        if crashed && before_time - *crash_timestamp >= Duration::seconds(5) {
-            let file_name = "edr.csv";
-            let _ = fs::remove_file(file_name);
-            let mut file = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .create_new(true)
-                .open(file_name)
-                .unwrap();
+        if crashed {
+            let before_time = event_data[event_data.len() - 1].timestamp;
 
-            if let Err(e) = writeln!(file, "TIMESTAMP,CRASHED,EVENT_NAME,VALUE") {
-                eprintln!("Couldn't write to file: {}", e);
-                return 2;
-            }
+            let crash_timestamp = event_data[crashed_index as usize].timestamp;
 
-            // loop event_data
-            for i in 0..event_data.len() {
-                let event_can_id = event_data[i].can_id;
-                if event_can_id == 0x1B4 {
+            if before_time - crash_timestamp >= Duration::seconds(5) {
+                let file_name = "edr.csv";
+                let _ = fs::remove_file(file_name);
+                let mut file = OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .create_new(true)
+                    .open(file_name)
+                    .unwrap();
+
+                if let Err(e) = writeln!(file, "TIMESTAMP,CRASHED,EVENT_NAME,VALUE") {
+                    eprintln!("couldn't write to file: {}", e);
+                    return 2;
+                }
+
+                // loop event_data
+                for i in 0..event_data.len() {
+                    let event_can_id = event_data[i].can_id;
                     // speed
                     if let Err(e) = writeln!(
                         file,
-                        "{},{},SPEED,{}",
-                        event_data[i].timestamp, event_data[i].crashed, event_data[i].speed,
+                        "{},{},{},{}",
+                        event_data[i].timestamp,
+                        if event_can_id == speed_id {
+                            "SPEED"
+                        } else if event_can_id == indicator_id {
+                            "INDICATOR"
+                        } else {
+                            "DOOR"
+                        },
+                        event_data[i].crashed,
+                        if event_can_id == speed_id {
+                            event_data[i].speed.to_string()
+                        } else if event_can_id == indicator_id {
+                            event_data[i].indicator.to_string()
+                        } else {
+                            event_data[i].door.to_string()
+                        },
                     ) {
-                        eprintln!("Couldn't write to file: {}", e);
-                        return 2;
-                    }
-                } else if event_can_id == 0x188 {
-                    // indicator
-                    if let Err(e) = writeln!(
-                        file,
-                        "{},{},INDICATOR,{}",
-                        event_data[i].timestamp, event_data[i].crashed, event_data[i].indicator
-                    ) {
-                        eprintln!("Couldn't write to file: {}", e);
-                        return 2;
-                    }
-                } else if event_can_id == 0x19b {
-                    // door
-                    if let Err(e) = writeln!(
-                        file,
-                        "{},{},DOOR,{}",
-                        event_data[i].timestamp, event_data[i].crashed, event_data[i].door
-                    ) {
-                        eprintln!("Couldn't write to file: {}", e);
+                        eprintln!("couldn't write speed data to file: {}", e);
                         return 2;
                     }
                 }
+                return 1;
             }
-            return 1;
         }
 
         let last_item_index = event_data.len() - 1;
@@ -112,7 +119,7 @@ pub extern "C" fn record_in_edr(
         let mut before_speed: f64 = now_speed;
 
         for i in (0..last_item_index).rev() {
-            if event_data[i].can_id == 0x1B4 {
+            if event_data[i].can_id == speed_id {
                 // speed
                 before_speed = event_data[i].speed;
                 break;
@@ -125,10 +132,8 @@ pub extern "C" fn record_in_edr(
         if speed_delta.abs() >= 10.0 {
             println!("crashed!!!!!");
             event_data[last_item_index].crashed = true;
-            *crash_timestamp = event_data[last_item_index].timestamp;
         }
-
-    } else if can_id == 0x188 {
+    } else if can_id == indicator_id {
         // indicator
         event_data.push(EventData {
             can_id: can_id,
@@ -138,7 +143,7 @@ pub extern "C" fn record_in_edr(
             indicator: indicator,
             door: 0,
         });
-    } else if can_id == 0x19B {
+    } else if can_id == door_id {
         // door
         event_data.push(EventData {
             can_id: can_id,
